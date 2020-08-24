802b6c09847f7cf1eb6f8a831128a61328381949
Pedro Cardoso <cardosoleonelpedro@gmail.com>
2020-08-24 18:59:18 -0300

    Métodos de filtros apagados

--- Catalogo.java
+++ Catalogo.java
@@ -1,34 +1,6 @@
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Catalogo {
-
-    public void filtrarCaminhoes() {
-        veiculosAVenda.stream().filter(c -> c instanceof Caminhao).forEach(System.out::println);
-    }
-
-    public void filtrarCarros() {
-        veiculosAVenda.stream().filter(c -> c instanceof Carro).forEach(System.out::println);
-    }
-
-    public void filtrarMotos() {
-        veiculosAVenda.stream().filter(c -> c instanceof Moto).forEach(System.out::println);
-    }
-
-    public void filtrarPopularidade() {
-        Collections.sort(veiculosAVenda, new OrdenaPorPopularidade());
-        veiculosAVenda.stream().forEach(System.out::println);
-    }
-
-    public void filtrarMaiorPreco() {
-        Collections.sort(veiculosAVenda, new OrdenaMaiorPreco());
-        veiculosAVenda.stream().forEach(System.out::println);
-    }
-
-    public void filtrarMenorPreco() {
-        Collections.sort(veiculosAVenda, new OrdenaMenorPreco());
-        veiculosAVenda.stream().forEach(System.out::println);
-    }
 
 }
