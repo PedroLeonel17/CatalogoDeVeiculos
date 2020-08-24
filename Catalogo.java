0529e0d0c5b84b78bb168e1054feec853acbe4d9
Benno <jbweber05@gmail.com>
2020-08-24 18:06:15 -0300

    Catalogo HardCoded

---
+++ Catalogo.java
@@ -0,0 +1,23 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class Catalogo {
+
+    public List<Veiculo> veiculosAVenda;
+
+    Catalogo() {
+        veiculosAVenda = new LinkedList<>();
+        veiculosAVenda.add(new Moto("GSX-R1000", 3, "Suzuki", 54880.00f));
+        veiculosAVenda.add(new Moto("CBR1000 RR - fireblade", 2, "Honda", 49750.00f));
+        veiculosAVenda.add(new Moto("YZF-R1", 4, "Yamaha", 62330.00f));
+        veiculosAVenda.add(new Moto("ZZR 1400 Ninja", 1, "Kawasaki", 74100.00f));
+        veiculosAVenda.add(new Carro("Fusca", 5, "Volkswagen", 10000.00f));
+        veiculosAVenda.add(new Carro("Uno Attractive", 5, "Fiat", 45890.00f));
+        veiculosAVenda.add(new Carro("HB20", 3, "Hyundai", 47990.00f));
+        veiculosAVenda.add(new Carro("Ka S", 3, "Ford", 49890.00f));
+        veiculosAVenda.add(new Carro("Gol", 3, "Volkswagen", 51090.00f));
+        veiculosAVenda.add(new Caminhao("HD80", 4, "Hyundai", 116100.00f));
+        veiculosAVenda.add(new Caminhao("Delivery Express 2p", 2, "Volkswagen", 123571.00f));
+        veiculosAVenda.add(new Caminhao("F-4000 4x2 2p", 3, "Ford", 136438.00f));
+    }
+}
