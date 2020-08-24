72477c96734affa5b219260e5c0cb32a747b5ee1
Benno <jbweber05@gmail.com>
2020-08-24 15:35:21 -0300

    Interface Veiculo Implementada

---
+++ Veiculo.java
@@ -0,0 +1,20 @@
+public interface Veiculo {
+
+    public void setNome(String nome);
+
+    public void setPopularidade(int popularidade);
+
+    public void setValor(float valor);
+
+    public void setMarca(String marca);
+
+    public String getNome();
+
+    public int getPopularidade();
+
+    public float getValor();
+
+    public String getMarca();
+
+    public String toString();
+}
